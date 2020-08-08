f52757b51ab395f97d58088c51868c63bf797ca6
anthopark <kunhpark15@gmail.com>
2020-08-07 17:49:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Traversy-Tasklist
---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#task-form');
+const taskCollection = document.querySelector('.collection');
+
+const handleAddTaskSumit = function (e) {
+    const inputTask = e.target.children[0].children[0].value
+
+    addToCollection(inputTask);
+    e.preventDefault();
+}
+
+function addToCollection(inputTask) {
+    const li = document.createElement("li");
+    li.classList.add("collection-item")
+    
+    li.innerHTML = `${inputTask}
+        <a href="#" class="delete-item secondary-content">
+            <i class="fa fa-remove"></i>
+         </a>`
+    
+    taskCollection.appendChild(li);
+
+    console.log(li)
+}
+
+
+form.addEventListener('submit', handleAddTaskSumit)
