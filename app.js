7f54080b0ee58299a58e836f7ddb2f1f8b25b8bc
Emily Phee <ephee@uci.edu>
2020-08-10 15:09:07 -0700

    Add filter tasks functionality on app.js

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const form = document.querySelector('#task-form');
 const taskCollection = document.querySelector('.collection');
+const filterTaskInput = document.querySelector('#filter');
+// const test = document.querySelector('#task');
+
+let taskArr = [];
+
 
 
 
 const handleAddTaskSumit = function (e) {
-    const inputTask = e.target.children[0].children[0].value
+    const inputTask = e.target.children[0].children[0].value;
+    e.target.children[0].children[0].value = '';
 
     addToCollection(inputTask);
     e.preventDefault();
+
+
+
 }
 
 function addToCollection(inputTask) {
     const li = document.createElement("li");
-    li.classList.add("collection-item")
+    li.classList.add("collection-item");
+
+    // test.remove();
+    
     
     li.innerHTML = `${inputTask}
         <a href="#" class="delete-item secondary-content">
@@ -20,8 +32,7 @@ function addToCollection(inputTask) {
          </a>`
     
     taskCollection.appendChild(li);
-
-    console.log(li)
+    taskArr.push(li);
 }
 
 // Add delete functionality
@@ -30,21 +41,57 @@ function handleRemoveTask(e){
     if (e.target.className === 'fa fa-remove'){
         const liToDelete = e.target.parentElement.parentElement;
         liToDelete.remove();
+        taskArr = taskArr.filter(function (li) {
+            return li !== liToDelete;
+        })
     }
 }
 
 // Clear Task
-const clearTasks = document.addEventListener('click', function(e){
+document.addEventListener('click', function(e){
     // console.log(e.target); // <a class="clear-tasks btn black" href="#">
-    const deleteAll = e.target;
-    if (deleteAll.classList.contains('clear-tasks')){
+    if (e.target.classList.contains('clear-tasks')){
 
         const taskLists = document.querySelector('.collection'); // parent of li elements
-        taskLists.innerHTML = '';
+        taskLists.innerHTML = ''; // delete all parent's children
         
     }
-})
+});
 
-form.addEventListener('submit', handleAddTaskSumit)
+// Filter Task
+filterTaskInput.addEventListener('input', handleFilterTask);
+
+
+function handleFilterTask(e){
+
+    console.log(e.target.value);
+
+    const userInput = e.target.value;
+
+    console.log(taskArr);
+
+    const results = taskArr.filter(function (li) {
+        return li.innerText.includes(userInput);
+    })
+
+    // console.log(results);
+
+    // console.log(taskCollection.children);
+    taskCollection.innerHTML = '';
+    results.forEach(function(li){
+        taskCollection.appendChild(li);
+    });
+    
+
+}
+
+form.addEventListener('submit', handleAddTaskSumit);
+
+
 taskCollection.addEventListener('click', handleRemoveTask);
+
+
+
+console.log(document.querySelector('.collection'));
+
 
