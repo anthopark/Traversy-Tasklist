29e7b23a2ab27b7e211a86c32154128987058b2f
Emily Phee <ephee@uci.edu>
2020-08-11 16:53:05 -0700

    Add handle local storage functionality and made a change of variables position

--- app.js
+++ app.js
@@ -1,51 +1,22 @@
 const form = document.querySelector('#task-form');
 const taskCollection = document.querySelector('.collection');
 const filterTaskInput = document.querySelector('#filter');
-// const test = document.querySelector('#task');
+
+form.addEventListener('submit', handleAddTaskSumit);
+taskCollection.addEventListener('click', handleRemoveTask);
+
+// test printing the local sotrage array
+let tasks = JSON.parse(localStorage.getItem('tasks'));
+if (tasks === null) {
+    tasks = []
+}
+
+// Add back to UI (that webpage)
+tasks.forEach(function(task){
+    addToCollection(task);
+});
 
 let taskArr = [];
-
-
-
-
-const handleAddTaskSumit = function (e) {
-    const inputTask = e.target.children[0].children[0].value;
-    e.target.children[0].children[0].value = '';
-
-    addToCollection(inputTask);
-    e.preventDefault();
-
-
-
-}
-
-function addToCollection(inputTask) {
-    const li = document.createElement("li");
-    li.classList.add("collection-item");
-
-    // test.remove();
-    
-    
-    li.innerHTML = `${inputTask}
-        <a href="#" class="delete-item secondary-content">
-            <i class="fa fa-remove"></i>
-         </a>`
-    
-    taskCollection.appendChild(li);
-    taskArr.push(li);
-}
-
-// Add delete functionality
-function handleRemoveTask(e){
-    console.log(e.target);
-    if (e.target.className === 'fa fa-remove'){
-        const liToDelete = e.target.parentElement.parentElement;
-        liToDelete.remove();
-        taskArr = taskArr.filter(function (li) {
-            return li !== liToDelete;
-        })
-    }
-}
 
 // Clear Task
 document.addEventListener('click', function(e){
@@ -54,12 +25,77 @@ document.addEventListener('click', function(e){
 
         const taskLists = document.querySelector('.collection'); // parent of li elements
         taskLists.innerHTML = ''; // delete all parent's children
-        
+
+        // Remove Array from local storage
+        localStorage.setItem('tasks', "[]");   
     }
 });
 
 // Filter Task
 filterTaskInput.addEventListener('input', handleFilterTask);
+
+function handleAddTaskSumit(e) {
+    const inputTask = e.target.children[0].children[0].value;
+    e.target.children[0].children[0].value = '';
+
+    addToCollection(inputTask);
+
+    // save tasks to array
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(inputTask);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+ 
+    e.preventDefault();
+}
+
+function addToCollection(inputTask) {
+    const li = document.createElement("li");
+    li.classList.add("collection-item");
+    
+    
+    li.innerHTML = `${inputTask}
+        <a href="#" class="delete-item secondary-content">
+            <i class="fa fa-remove"></i>
+         </a>
+    `;
+    
+    taskCollection.appendChild(li);
+    taskArr.push(li);
+}
+
+
+
+// Add remove functionality
+function handleRemoveTask(e){
+    console.log(e.target);
+    if (e.target.className === 'fa fa-remove'){
+        const liToDelete = e.target.parentElement.parentElement;
+        liToDelete.remove();
+
+
+        taskArr = taskArr.filter(function (li) {
+            return li !== liToDelete;
+        })
+        console.log(taskArr);
+
+        const afterDeletionArr = []
+
+        taskArr.forEach(function (task) {
+            afterDeletionArr.push(task.innerText);
+        });
+
+        localStorage.setItem('tasks', JSON.stringify(afterDeletionArr));
+
+    }
+} 
+
+
 
 
 function handleFilterTask(e){
@@ -85,13 +121,6 @@ function handleFilterTask(e){
 
 }
 
-form.addEventListener('submit', handleAddTaskSumit);
 
-
-taskCollection.addEventListener('click', handleRemoveTask);
-
-
-
-console.log(document.querySelector('.collection'));
 
 
